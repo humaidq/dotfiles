a4d0185bb534d76c1edd420ed532d95ac829603a
Humaid Alqasimi <git@huma.id>
2024-08-22 16:41:56 +0400

    Add hydra jobs
    Signed-off-by: Humaid Alqasimi <git@huma.id>

--- hosts/default.nix
+++ hosts/default.nix
@@ -6,106 +6,123 @@
   ...
 }:
 {
-  flake.nixosModules = {
-    host-goral = import ./goral;
-    host-serow = import ./serow;
-    host-duisk = import ./duisk;
-    host-tahr = import ./tahr;
-    host-boerbok = import ./boerbok;
-    host-argali = import ./argali;
-    host-arkelli = import ./arkelli;
+  flake = {
+    nixosModules = {
+      host-goral = import ./goral;
+      host-serow = import ./serow;
+      host-duisk = import ./duisk;
+      host-tahr = import ./tahr;
+      host-boerbok = import ./boerbok;
+      host-argali = import ./argali;
+      host-arkelli = import ./arkelli;
 
-    # Generators hosts
-    host-rpi4-bootstrap = import ./rpi4-bootstrap.nix;
-    host-rpi5-bootstrap = import ./rpi5-bootstrap.nix;
-    host-x86-installer = import ./x86-installer.nix;
-  };
-  flake.nixosConfigurations =
-    let
-      specialArgs = {
-        inherit self inputs vars;
+      # Generators hosts
+      host-rpi4-bootstrap = import ./rpi4-bootstrap.nix;
+      host-rpi5-bootstrap = import ./rpi5-bootstrap.nix;
+      host-x86-installer = import ./x86-installer.nix;
+    };
+    nixosConfigurations =
+      let
+        specialArgs = {
+          inherit self inputs vars;
+        };
+      in
+      {
+        goral = lib.nixosSystem {
+          inherit specialArgs;
+          modules = [ self.nixosModules.host-goral ];
+        };
+        serow = lib.nixosSystem {
+          inherit specialArgs;
+          modules = [ self.nixosModules.host-serow ];
+        };
+        duisk = lib.nixosSystem {
+          inherit specialArgs;
+          modules = [ self.nixosModules.host-duisk ];
+        };
+        tahr = lib.nixosSystem {
+          inherit specialArgs;
+          modules = [ self.nixosModules.host-tahr ];
+        };
+        boerbok = lib.nixosSystem {
+          inherit specialArgs;
+          modules = [ self.nixosModules.host-boerbok ];
+        };
+        argali = lib.nixosSystem {
+          inherit specialArgs;
+          modules = [ self.nixosModules.host-argali ];
+        };
+        arkelli = lib.nixosSystem {
+          inherit specialArgs;
+          modules = [ self.nixosModules.host-arkelli ];
+        };
       };
-    in
-    {
-      goral = lib.nixosSystem {
-        inherit specialArgs;
-        modules = [ self.nixosModules.host-goral ];
+
+    packages.x86_64-linux =
+      let
+        specialArgs = {
+          inherit self inputs vars;
+        };
+      in
+      {
+        installer = inputs.nixos-generators.nixosGenerate {
+          format = "iso";
+          system = "x86_64-linux";
+          inherit specialArgs;
+          modules = [
+            self.nixosModules.host-x86-installer
+            {
+              isoImage = {
+                squashfsCompression = "zstd -Xcompression-level 6";
+              };
+            }
+          ];
+        };
       };
-      serow = lib.nixosSystem {
-        inherit specialArgs;
-        modules = [ self.nixosModules.host-serow ];
+
+    packages.aarch64-linux =
+      let
+        specialArgs = {
+          inherit self inputs vars;
+        };
+      in
+      {
+        rpi4-bootstrap = inputs.nixos-generators.nixosGenerate {
+          format = "sd-aarch64";
+          system = "aarch64-linux";
+          inherit specialArgs;
+          modules = [
+            self.nixosModules.host-rpi4-bootstrap
+            { sdImage.compressImage = false; }
+          ];
+        };
+        rpi5-bootstrap = inputs.nixos-generators.nixosGenerate {
+          format = "sd-aarch64";
+          system = "aarch64-linux";
+          inherit specialArgs;
+          modules = [
+            self.nixosModules.host-rpi5-bootstrap
+            { sdImage.compressImage = false; }
+          ];
+        };
       };
-      duisk = lib.nixosSystem {
-        inherit specialArgs;
-        modules = [ self.nixosModules.host-duisk ];
-      };
-      tahr = lib.nixosSystem {
-        inherit specialArgs;
-        modules = [ self.nixosModules.host-tahr ];
-      };
-      boerbok = lib.nixosSystem {
-        inherit specialArgs;
-        modules = [ self.nixosModules.host-boerbok ];
-      };
-      argali = lib.nixosSystem {
-        inherit specialArgs;
-        modules = [ self.nixosModules.host-argali ];
-      };
-      arkelli = lib.nixosSystem {
-        inherit specialArgs;
-        modules = [ self.nixosModules.host-arkelli ];
-      };
+
+    packages.riscv64-linux = {
+      boerbok-sd = self.nixosConfigurations.boerbok.config.system.build.sdImage;
     };
 
-  flake.packages.x86_64-linux =
-    let
-      specialArgs = {
-        inherit self inputs vars;
-      };
-    in
-    {
-      installer = inputs.nixos-generators.nixosGenerate {
-        format = "iso";
-        system = "x86_64-linux";
-        inherit specialArgs;
-        modules = [
-          self.nixosModules.host-x86-installer
-          {
-            isoImage = {
-              squashfsCompression = "zstd -Xcompression-level 6";
-            };
-          }
-        ];
-      };
-    };
+    hydraJobs = {
+      serow = self.nixosConfigurations.serow.config.system.build.toplevel;
+      tahr = self.nixosConfigurations.tahr.config.system.build.toplevel;
+      duisk = self.nixosConfigurations.duisk.config.system.build.toplevel;
+      #goral = self.nixosConfigurations.goral.config.system.build.toplevel;
+      boerbok = self.nixosConfigurations.boerbok.config.system.build.toplevel;
+      argali = self.nixosConfigurations.argali.config.system.build.toplevel;
+      arkelli = self.nixosConfigurations.arkelli.config.system.build.toplevel;
 
-  flake.packages.aarch64-linux =
-    let
-      specialArgs = {
-        inherit self inputs vars;
-      };
-    in
-    {
-      rpi4-bootstrap = inputs.nixos-generators.nixosGenerate {
-        format = "sd-aarch64";
-        system = "aarch64-linux";
-        inherit specialArgs;
-        modules = [
-          self.nixosModules.host-rpi4-bootstrap
-          { sdImage.compressImage = false; }
-        ];
-      };
-      rpi5-bootstrap = inputs.nixos-generators.nixosGenerate {
-        format = "sd-aarch64";
-        system = "aarch64-linux";
-        inherit specialArgs;
-        modules = [
-          self.nixosModules.host-rpi5-bootstrap
-          { sdImage.compressImage = false; }
-        ];
-      };
+      boerbok-sd = self.packages.riscv64-linux.boerbok-sd;
+      rpi4-bootstrap = self.packages.aarch64-linux.rpi4-bootstrap;
+      rpi5-bootstrap = self.packages.aarch64-linux.rpi5-bootstrap;
     };
-  flake.packages.riscv64-linux = {
-    boerbok-sd = self.nixosConfigurations.boerbok.config.system.build.sdImage;
   };
 }
