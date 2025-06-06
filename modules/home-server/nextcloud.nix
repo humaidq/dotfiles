6cd63a25e24d4ff1cd3da1037f813e190569b23a
Humaid Alqasimi <me@humaidq.ae>
2025-06-06 09:49:37 +0400

    nextcloud: update package

--- modules/home-server/nextcloud.nix
+++ modules/home-server/nextcloud.nix
@@ -16,7 +16,7 @@ in
     };
     services.nextcloud = {
       enable = true;
-      package = pkgs.nextcloud30;
+      package = pkgs.nextcloud31;
       hostName = "cloud.alq.ae";
       config.dbtype = "sqlite";
       config.adminpassFile = config.sops.secrets."nextcloud/adminpass".path;
