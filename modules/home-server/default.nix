eb8358b8b637ee28c4e76e8c03b7ebe5df3b3a17
Humaid Alqasimi <me@humaidq.ae>
2025-06-06 09:35:47 +0400

    derive home service domains

--- modules/home-server/default.nix
+++ modules/home-server/default.nix
@@ -1,4 +1,7 @@
-{ lib, inputs, ... }:
+{ lib, inputs, config, ... }:
+let
+  cfg = config.sifr.home-server;
+in
 {
   imports = [
     inputs.authentik-nix.nixosModules.default
@@ -19,5 +22,17 @@
 
   options.sifr.home-server = {
     enable = lib.mkEnableOption "home server setup";
+    domains = lib.mkOption {
+      type = with lib.types; listOf str;
+      readOnly = true;
+      default = [];
+      description = "Domains served by the home server web instance.";
+    };
+  };
+
+  config = {
+    sifr.home-server.domains = lib.mkIf cfg.enable (
+      builtins.attrNames config.services.nginx.virtualHosts
+    );
   };
 }
