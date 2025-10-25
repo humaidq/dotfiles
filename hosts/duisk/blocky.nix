faa31b19c396e45daf745b0bd3b5c89de7d29280
Humaid Alqasimi <git@huma.id>
2025-10-25 19:33:28 +0400

    Fix blocky doh
    Signed-off-by: Humaid Alqasimi <git@huma.id>

--- hosts/duisk/blocky.nix
+++ hosts/duisk/blocky.nix
@@ -1,9 +1,30 @@
-_: {
+{ lib, ... }:
+{
   config = {
+    users.groups.blocky = { };
+    users.users.blocky = {
+      isSystemUser = true;
+      group = "blocky";
+      extraGroups = [ "caddy" ];
+    };
     systemd.services.blocky.serviceConfig = {
+      DynamicUser = lib.mkForce false;
+      User = "blocky";
+      Group = "blocky";
       SupplementaryGroups = [ "caddy" ];
     };
-
+    systemd.tmpfiles.rules = [
+      "d /var/lib/caddy 0750 caddy caddy - -"
+      "d /var/lib/caddy/.local 0750 caddy caddy - -"
+      "d /var/lib/caddy/.local/share 0750 caddy caddy - -"
+      "d /var/lib/caddy/.local/share/caddy 0750 caddy caddy - -"
+      "d /var/lib/caddy/.local/share/caddy/certificates 0750 caddy caddy - -"
+      "d /var/lib/caddy/.local/share/caddy/certificates/acme-v02.api.letsencrypt.org-directory 0750 caddy caddy - -"
+      "d /var/lib/caddy/.local/share/caddy/certificates/acme-v02.api.letsencrypt.org-directory/dns.huma.id 0750 caddy caddy - -"
+      "f /var/lib/caddy/.local/share/caddy/certificates/acme-v02.api.letsencrypt.org-directory/dns.huma.id/dns.huma.id.crt 0640 caddy caddy - -"
+      "f /var/lib/caddy/.local/share/caddy/certificates/acme-v02.api.letsencrypt.org-directory/dns.huma.id/dns.huma.id.key 0640 caddy caddy - -"
+    ];
+    systemd.services.caddy.serviceConfig.UMask = "0027";
     networking.firewall.allowedTCPPorts = [
       853
     ];
@@ -11,7 +32,7 @@ _: {
       enable = true;
       settings = {
         ports = {
-          dns = 53;
+          dns = 1153;
           http = 3333;
           https = 4333;
           tls = 853;
